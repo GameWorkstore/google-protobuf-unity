27123c9995e5dfa4af2c2c900bbb507a0e569bc2
HanzaRu <john.rosa@aquiris.com.br>
2020-11-30 10:27:26 -0300

    +Initial Commit

---
+++ Editor/PackageUpdater.cs
@@ -0,0 +1,16 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditor.PackageManager;
+
+namespace GameWorkstore.Google.Protobuf
+{
+    public class PackageUpdater
+    {
+        [MenuItem("Help/PackageUpdate/GameWorkstore.GoogleProtobufCsharp")]
+        public static void TrackPackages()
+        {
+            Client.Add("git://github.com:GameWorkstore/googleprotobufcsharp.git");
+        }
+    }
+}
+#endif
