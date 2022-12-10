674a42720342f946f2b38f61802e22576f092628
John Willy de Souza Rosa <woodimbr@gmail.com>
2022-12-10 16:44:51 +0100

    Update README.md

---
+++ Runtime/Google.Protobuf.Config/ProtobufConfig.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+namespace Google.Protobuf.Config
+{
+    [CreateAssetMenu(fileName = "ProtobufConfig", menuName = "Protobuf/ProtobufConfig")]
+    public class ProtobufConfig : ScriptableObject
+    {
+        public bool ProtocolCompilerEnabled = true;
+
+        [Header("C#")]
+        public bool CSharpCompilerEnabled = true;
+        [Tooltip("Leave custom paths empty if it should compile right next to original.")]
+        public string CSharpCustomPath = string.Empty;
+
+        [Header("GoLang#")]
+        public bool GoLangCompilerEnabled = false;
+        [Tooltip("Leave custom paths empty if it should compile right next to original.")]
+        public string GoLangCustomPath = string.Empty;
+
+        [Header("Python#")]
+        public bool PythonCompilerEnabled = false;
+        [Tooltip("Make protobuf local inside python exportations. [ 'from google' --> 'from . google'] and [ 'import' --> 'from . import']")]
+        public bool PythonLocalLibrary = false;
+        [Tooltip("Leave custom paths empty if it should compile right next to original.")]
+        public string PythonCustomPath = string.Empty;
+
+        [Header("Cpp#")]
+        public bool CppCompilerEnabled = false;
+        [Tooltip("Leave custom paths empty if it should compile right next to original.")]
+        public string CppCustomPath = string.Empty;
+    }
+}
